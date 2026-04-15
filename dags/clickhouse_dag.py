36507339125316b91e0bd5be0dba5e2a5df5c7d7
blacklycoriss <prokopovaleksandr544@gmail.com>
2026-04-16 01:40:44 +0700

    загружает все данные ежедневно

--- dags/clickhouse_dag.py
+++ dags/clickhouse_dag.py
@@ -6,14 +6,6 @@ from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook
 from airflow.utils.trigger_rule import TriggerRule
 import logging
 from datetime import datetime, timedelta
-  
-def get_max_date(**context):
-    ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_db')
-
-    max_date = ch_hook.execute("SELECT MAX(date) FROM ch_yf_data")[0][0]
-    if max_date is None:
-        max_date='2026-01-01'
-    context['ti'].xcom_push(key='max_date', value=max_date)
 
 #-------------------------------------------------------------------------------
 default_args = {
@@ -37,10 +29,7 @@ with DAG(
 ) as dag:
 
     start_task = EmptyOperator(task_id='start')
-    get_max_date_task = PythonOperator(
-        task_id='get_max_date_task',
-        python_callable=get_max_date
-    )
+
     incremental_load = ClickHouseOperator(
     task_id='incremental_load',
     clickhouse_conn_id='clickhouse_db',
@@ -49,10 +38,9 @@ with DAG(
         SELECT s.id, s.date, s.open, s.high, s.low, s.close, s.adj_close, s.volume
         FROM raw_pg_data AS s
         LEFT ANTI JOIN ch_yf_data AS t ON s.id = t.id AND s.date = t.date
-        WHERE s.date > toDate('{{ ti.xcom_pull(task_ids="get_max_date_task", key="max_date") }}')
     """,
-)
+    )
     
     end_task = EmptyOperator(task_id='end')
 
-    start_task >> get_max_date_task >> incremental_load >> end_task
+    start_task >> incremental_load >> end_task
